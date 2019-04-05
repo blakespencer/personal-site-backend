ce9679aa3cda138f9121014f9859bfee241a6a01
Blake Spencer <blake.spencer1@gmail.com>
2019-04-05 08:48:27 -0400

    [feature] backend search now avalible

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import os
-from flask import Flask, send_from_directory, jsonify
+from flask import Flask, send_from_directory, jsonify, request
 import pickle
+from spotipy.oauth2 import SpotifyClientCredentials
+from secrets import spotify_information
+import spotipy
+import spotipy.util as util
+import pandas as pd
+import urllib
+
 
 app = Flask(__name__, static_folder='react_app/build')
 
@@ -43,6 +50,51 @@ def get_classification_data() -> str:
         'precision': precision_data,
         'wrongPrediction': prediction_object
     }), 200
+
+
+client_credentials_manager = SpotifyClientCredentials(
+    client_id=spotify_information['client_id'], client_secret=spotify_information['client_secret'])
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+def get_spotify_row(uri):
+    track = sp.track(uri)
+    features = sp.audio_features(tracks=[uri])
+    analysis = sp.audio_analysis(uri)
+    track['album']['artists'][0]['track'] = track['name']
+    track = pd.DataFrame(track['album']['artists'][0])[
+        ['name', 'track']].reset_index()
+
+    features = pd.DataFrame(features)
+
+    analysis = analysis['track']
+    analysis['codestring'] = None
+    analysis['echoprintstring'] = None
+    analysis['synchstring'] = None
+    analysis['rhythmstring'] = None
+
+    analysis = pd.DataFrame(analysis, index=[0])
+
+    feat_ana = pd.merge(analysis, features)
+    feat_ana = feat_ana._get_numeric_data()
+
+    return pd.merge(feat_ana, track, how='left',  left_index=True, right_index=True).drop('index', axis=1)
+
+
+@app.route('/api/search')  # the site to route to, index/main in this case
+def search() -> str:
+    try:
+        prediction = jsonify(message='20000'), 200
+        uri = request.args.get('query')
+        if(uri is not None):
+            query = urllib.parse.unquote(uri)
+            response = sp.search(query, limit=12)
+            tracks = response['tracks']['items']
+            prediction = jsonify(tracks), 200
+        return prediction
+    except Exception as e:
+        print(e)
+        return jsonify([]), 404
 
 
 @app.route('/', defaults={'path': ''})
---
+++ secrets.py
@@ -0,0 +1,2 @@
+spotify_information = {'client_id': 'db295d556f064264821d76c9178a940f',
+                       'client_secret': '1b9aa7a8ad6e4f79950d566d80725740'}
